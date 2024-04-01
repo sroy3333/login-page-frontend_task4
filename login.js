07eb859cc70bea742f4fa00184faf4f1d47a359b
sroy3333 <121767970+sroy3333@users.noreply.github.com>
2024-04-01 13:27:43 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,20 +1,18 @@
 async function login(e) {
-    try {
-        e.preventDefault();
+    e.preventDefault();
+    console.log(e.target.name);
 
-        const loginDetails = {
-            email: e.target.email.value,
-            password: e.target.password.value
-        }
-
-        const response = await axios.post('http://localhost:5000/user/login', loginDetails);
-
-        if(response.status === 200) {
-            window.location.href = "../Dashboard/dashboard.html";
-        } else {
-            throw new Error('Failed to login');
-        }
-    } catch(err) {
-        document.body.innerHTML += `<div style="color:red;">${err} <div>`;
+    const loginDetails = {
+        email: e.target.email.value,
+        password: e.target.password.value
+        
     }
+    console.log(loginDetails);
+    await axios.post('http://localhost:5000/user/login', loginDetails).then(response => {
+            alert(response.data.message)
+    }).catch(err => {
+        console.log(json.stingify(err))
+        document.body.innerHTML += `<div style="color:red;">${err} <div>`;
+    })
+
 }
